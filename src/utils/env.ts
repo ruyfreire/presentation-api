964a1635f38153dd754d635c58315f047311a663
Ruy Freire <ruy.slv@gmail.com>
2026-06-11 12:37:01 -0300

    chore: add HOST environment variable and update server listening configuration

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,6 +2,7 @@ import z from 'zod'
 
 export const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
+  HOST: z.string().default('0.0.0.0'),
   MONGODB_URI: z.string({
     error: 'MONGODB_URI environment is required',
   }),
