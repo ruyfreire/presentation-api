3956e7f91df50b4f170ee170a5c4933e699a7ceb
Ruy Freire <ruy.slv@gmail.com>
2026-08-22 16:27:14 -0300

    feat: update get profile to public route

--- src/modules/profile/controllers/get-profile.controller.ts
+++ src/modules/profile/controllers/get-profile.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Query } from '@nestjs/common'
 
+import { Public } from '../../auth/auth.decorators'
 import { GetProfileService } from '../services/get-profile.service'
 
 @Controller('profile')
 export class GetProfileController {
   constructor(private readonly getProfileService: GetProfileService) {}
 
+  @Public()
   @Get('')
   getProfile(@Query('profileId') profileId: string) {
     return this.getProfileService.execute(profileId ?? 'default')
